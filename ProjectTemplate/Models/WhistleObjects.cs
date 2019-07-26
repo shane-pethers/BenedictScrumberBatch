509afe30a7884416af4adceab292d1972239e930
Shane <ASAP_shark@outlook.com>
2019-07-25 20:19:15 -0700

    added database connection string, and created the models class to hold our attributes

---
+++ ProjectTemplate/Models/WhistleObjects.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate.Models
+{
+    public class WhistleObjects
+    {
+        public int PostID { get; set; }
+        public string Desc { get; set; }
+        public string PostText { get; set; }
+        public int PostVotes {get; set;}
+        public bool Flag { get; set; }
+        public string Category { get; set; }
+        public int UserID { get; set; }
+        public string Password { get; set; }
+    }
+}
